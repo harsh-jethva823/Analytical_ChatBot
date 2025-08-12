cf8e76667bb8c6d2ce0d304041c8f034fa63caf5
harsh-jethva823 <107455009+harsh-jethva823@users.noreply.github.com>
2025-08-12 21:18:57 +0530

    Delete prooject directory

--- prooject/college_chatbot/api_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import api_views
-
-urlpatterns = [
-    path('chat/', api_views.chat_message, name='chat_message'),
-    path('conversation/<str:session_id>/', api_views.get_conversation, name='get_conversation'),
-]
--- prooject/college_chatbot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CollegeChatbotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'college_chatbot'
